c1999aacd512b22733be51ef0d4776d32c2368ce
bkyuchuk <bogdankyuchuk@gmail.com>
2021-07-13 18:06:10 +0200

    Initial commit. Started argparse tutorial. Added steps for tests

---
+++ tests/passgen_test.py
@@ -0,0 +1,7 @@
+# When only 'passgen' is typed, the program should output:
+#   (in purple)Generated Password: (in green)I!!^Cz%%
+#   (in yellow)Password copied to clipboard.
+
+# 1. The program should generate an 8 char password.
+# 2. The password should contain letters and special chars.
+# 3. The password should be copied to the clipboard.
